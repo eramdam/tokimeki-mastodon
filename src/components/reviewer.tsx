25583870c9269beeb32b5e1b5df1e7509321d1a3
Damien Erambert <damien@erambert.me>
2022-12-27 21:14:12 +0100

    tweak animation stuff

--- src/components/reviewer.tsx
+++ src/components/reviewer.tsx
@@ -3,6 +3,7 @@ import parse from "html-react-parser";
 import type { mastodon } from "masto";
 import { login } from "masto";
 import { useCallback, useEffect, useMemo, useState } from "react";
+import { delayAsync } from "../helpers/asyncHelpers";
 import { getFollowings } from "../helpers/mastodonHelpers";
 import { useItemFromLocalForage } from "../helpers/storageHelpers";
 import { Button, SmallButton } from "./button";
@@ -15,6 +16,7 @@ enum AnimationState {
   Idle,
   Unfollow,
   Keep,
+  Hidden,
 }
 
 export function Reviewer() {
@@ -46,6 +48,98 @@ export function Reviewer() {
     [currentAccount?.emojis]
   );
 
+  function renderPrompt() {
+    if (!currentAccount) {
+      return null;
+    }
+    if (animationState === AnimationState.Keep) {
+      return (
+        <p className="prose leading-tight">
+          Glad to hear{" "}
+          <strong>
+            {renderWithEmoji(currentAccount.emojis, currentAccount.displayName)}
+          </strong>
+          &apos;s toots are still important to you.
+        </p>
+      );
+    }
+
+    if (animationState === AnimationState.Unfollow) {
+      return (
+        <p className="prose leading-tight">
+          Great, unfollowed! Let&apos;s thank{" "}
+          <strong>
+            {renderWithEmoji(currentAccount.emojis, currentAccount.displayName)}
+          </strong>{" "}
+          for all the posts you&apos;ve enjoyed before.{" "}
+        </p>
+      );
+    }
+
+    if (animationState === AnimationState.Hidden) {
+      return <p className="prose leading-tight">Loading...</p>;
+    }
+
+    return (
+      <p className="prose leading-tight">
+        Do their posts still spark joy or feel important to you?
+      </p>
+    );
+  }
+
+  function renderFooter() {
+    if (!currentAccount) {
+      return null;
+    }
+
+    if (animationState === AnimationState.Idle) {
+      return (
+        <>
+          <Button
+            variant="secondary"
+            onPress={() => {
+              setAnimated(AnimationState.Unfollow);
+            }}
+          >
+            Unfollow
+          </Button>
+          <Button
+            onPress={() => {
+              setAnimated(AnimationState.Keep);
+            }}
+            variant="secondary"
+          >
+            Keep
+          </Button>
+        </>
+      );
+    }
+
+    return (
+      <>
+        <Button
+          variant="secondary"
+          onPress={() => {
+            setAnimated(AnimationState.Idle);
+          }}
+        >
+          Undo
+        </Button>
+        <Button
+          onPress={async () => {
+            setAnimated(AnimationState.Hidden);
+            setIndex((p) => p + 1);
+            await delayAsync(600);
+            setAnimated(AnimationState.Idle);
+          }}
+          variant="secondary"
+        >
+          Next
+        </Button>
+      </>
+    );
+  }
+
   if (accounts.length === 0 || !masto) {
     return (
       <Block>
@@ -64,7 +158,9 @@ export function Reviewer() {
         className={clsx(
           "m-auto inline-flex flex-1 flex-shrink overflow-hidden p-0",
           "w-[400px]",
-          "scale-1 transition-all duration-[250ms] ease-in-out",
+          "opacity-0 transition-all duration-[250ms] ease-in-out",
+          animationState === AnimationState.Hidden && "scale-0",
+          animationState === AnimationState.Idle && "scale-1 opacity-100",
           animationState === AnimationState.Keep &&
             "translate-x-[20%] translate-y-[200px] rotate-[10deg] scale-[0.5] opacity-0",
           animationState === AnimationState.Unfollow &&
@@ -78,64 +174,43 @@ export function Reviewer() {
         ></FeedWidget>
       </Block>
       <Block className="mt-0 flex w-3/4 flex-shrink-0 flex-col items-start">
-        <div className="flex w-full items-center justify-between ">
-          <p className="prose break-words text-left leading-tight">
-            {index === 0 ? "Starting with" : `#${index + 1}:`}{" "}
-            <strong>
-              {renderWithEmoji(
-                currentAccount.emojis,
-                currentAccount.displayName
-              )}
-            </strong>{" "}
-            <a
-              href={currentAccount.url}
-              target="_blank"
-              className="text-sm text-neutral-400 hover:underline"
-              rel="noreferrer noopener"
-            >
-              @{currentAccount.acct}!
-            </a>
-          </p>
-          <SmallButton
-            variant="secondary"
-            onPress={() => setShowBio((p) => !p)}
-          >
-            {showBio ? "Hide bio" : "Show bio"}
-          </SmallButton>
-        </div>
-        {showBio && (
-          <div className="leading-tight">
-            {parse(currentAccount.note, parseOptions)}
-          </div>
+        {animationState === AnimationState.Idle && (
+          <>
+            <div className="flex w-full items-center justify-between ">
+              <p className="prose break-words text-left leading-tight">
+                {index === 0 ? "Starting with" : `#${index + 1}:`}{" "}
+                <strong>
+                  {renderWithEmoji(
+                    currentAccount.emojis,
+                    currentAccount.displayName
+                  )}
+                </strong>{" "}
+                <a
+                  href={currentAccount.url}
+                  target="_blank"
+                  className="text-sm text-neutral-400 hover:underline"
+                  rel="noreferrer noopener"
+                >
+                  @{currentAccount.acct}!
+                </a>
+              </p>
+              <SmallButton
+                variant="secondary"
+                onPress={() => setShowBio((p) => !p)}
+              >
+                {showBio ? "Hide bio" : "Show bio"}
+              </SmallButton>
+            </div>
+            {showBio && (
+              <div className="leading-tight">
+                {parse(currentAccount.note, parseOptions)}
+              </div>
+            )}
+          </>
         )}
-        <p className="prose leading-tight">
-          Do their posts still spark joy or feel important to you?
-        </p>
-        <div className="mt-2 -mb-8 inline-flex gap-4">
-          <Button
-            variant="primary"
-            onPress={() => {
-              setAnimated(AnimationState.Unfollow);
-              setIndex((p) => p + 1);
-              setTimeout(() => {
-                setAnimated(AnimationState.Idle);
-              }, 500);
-            }}
-          >
-            Unfollow
-          </Button>
-          <Button
-            onPress={() => {
-              setAnimated(AnimationState.Keep);
-              setIndex((p) => p + 1);
-              setTimeout(() => {
-                setAnimated(AnimationState.Idle);
-              }, 500);
-            }}
-            variant="secondary"
-          >
-            Keep
-          </Button>
+        <p className="prose leading-tight">{renderPrompt()}</p>
+        <div className="mt-2 -mb-8 inline-flex w-full justify-center gap-4">
+          {renderFooter()}
         </div>
       </Block>
     </div>
