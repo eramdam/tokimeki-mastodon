04868d745379acb73c538e8e876b2a40d063bf5b
Damien Erambert <damien@erambert.me>
2022-12-29 05:43:06 -0800

    :wrench:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,10 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import Head from "next/head";
-import { useEffect } from "react";
 import { SSRProvider } from "react-aria";
-import { initLocalForage } from "../helpers/storageHelpers";
 
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  useEffect(() => {
-    initLocalForage();
-  }, []);
-
   return (
     <SSRProvider>
       <Head>
