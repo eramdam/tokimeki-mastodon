7828aace1fdf261e7977850c3e16c42732a95c91
Damien Erambert <damien@erambert.me>
2022-12-31 08:49:19 -0800

    :wrench:

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -9,6 +9,7 @@ export function Block(props: PropsWithHtmlProps<"div">) {
         "my-10 mx-auto max-w-2xl gap-3 rounded-lg p-4 shadow-xl",
         "bg-white",
         "dark:bg-neutral-800",
+        "peer peer-[]:-mt-4",
         className
       )}
       {...rest}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -118,52 +118,88 @@ const Home: NextPage = () => {
   };
 
   return (
-    <Block className="flex flex-col items-center justify-center">
-      <Head>
-        <title>Tokimeki Mastodon</title>
-        <meta name="description" content="" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1 className="text-accentColor text-center">
-        ✨ Welcome to Tokimeki Mastodon ✨
-      </h1>
-      <p className="prose dark:prose-invert">
-        Following too many accounts? You&apos;re in the right place!
-      </p>
-      <p className="prose dark:prose-invert">
-        This tool is inspired by{" "}
-        <a href="https://tokimeki-unfollow.glitch.me/">Tokimeki Unfollow</a>{" "}
-        which has become my preferred way to clean my Twitter followings.
-        Unfortunately there was no equivalent for Mastodon... Until now!
-      </p>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          onLogin();
-        }}
-        className="my-10 flex w-full max-w-lg flex-col gap-5"
-      >
-        <TextInput
-          label="Instance URL"
-          placeholder="https://"
-          type={"url"}
-          className="prose flex flex-col gap-2 text-center dark:prose-invert"
-          value={instanceUrl}
-          onChange={setInstanceDomain}
-          validationState={isInstanceValid || "valid"}
-          isDisabled={isLoading}
-        ></TextInput>
-        <div className="flex justify-center">
-          <Button
-            variant="primary"
-            type="submit"
-            isDisabled={isInstanceValid === "invalid" || isLoading}
-          >
-            {(isLoading && "Loading...") || "Login"}
-          </Button>
+    <>
+      <Block className="flex flex-col items-center justify-center">
+        <Head>
+          <title>Tokimeki Mastodon</title>
+          <meta name="description" content="" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <h1 className="text-accentColor text-center">
+          ✨ Welcome to Tokimeki Mastodon ✨
+        </h1>
+        <p className="prose dark:prose-invert">
+          Following too many accounts? You&apos;re in the right place!
+        </p>
+        <p className="prose dark:prose-invert">
+          If you&apos;re like me, you have followed a lot of accounts over the
+          years on Mastodon. Some of them date from years ago, you were a
+          different human being! Some of them you feel like you *have* to keep
+          following, and others you may have outgrown, but you never had the
+          energy to clean up your follows.
+        </p>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            onLogin();
+          }}
+          className="my-10 flex w-full max-w-lg flex-col gap-5"
+        >
+          <TextInput
+            label="Instance URL"
+            placeholder="https://"
+            type={"url"}
+            className="prose flex flex-col gap-2 text-center dark:prose-invert"
+            value={instanceUrl}
+            onChange={setInstanceDomain}
+            validationState={isInstanceValid || "valid"}
+            isDisabled={isLoading}
+          ></TextInput>
+          <div className="flex justify-center">
+            <Button
+              variant="primary"
+              type="submit"
+              isDisabled={isInstanceValid === "invalid" || isLoading}
+            >
+              {(isLoading && "Loading...") || "Login"}
+            </Button>
+          </div>
+        </form>
+      </Block>
+      <Block>
+        <div className="prose opacity-60 dark:prose-invert">
+          <p>
+            P.S. Tokimeki is the original word that was translated to
+            &quot;spark joy&quot; in English. &quot;Spark joy&quot; doesn&apos;t
+            fully capture the meaning, which is why there are all these caveats
+            whenever someone explains it, like I&apos;m doing here. Please treat
+            this term as inclusive of anything you enjoy or feel is important to
+            you. Also, KonMari (TM) is trademark of Marie Kondo, and this tool
+            is not affiliated with, nor does it profit off the use of the brand.
+          </p>
+          <p>
+            P.P.S. This tool uses your browser&apos;s local storage (not
+            cookies) to store your progress. This tool uses your Mastodon&apos;s
+            account authorization to fetch your followings, their toots and
+            unfollow accounts. The code is{" "}
+            <a href="https://github.com/eramdam/tokimeki-mastodon">
+              open source and hosted on GitHub.
+            </a>
+          </p>
+          <p>
+            Based off{" "}
+            <a href="https://tokimeki-unfollow.glitch.me/">Tokimeki Unfollow</a>{" "}
+            by <a href="https://tarng.com/">Julius Tarng</a>.
+            <br />
+            <br />
+            Made by <a href="https://erambert.me">Damien Erambert</a>. Find me
+            at{" "}
+            <a href="https://octodon.social/@eramdam">eramdam@octodon.social</a>
+            !
+          </p>
         </div>
-      </form>
-    </Block>
+      </Block>
+    </>
   );
 };
 
