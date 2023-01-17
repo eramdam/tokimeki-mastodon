98d118a57a31256a8e47dcf933f529327828cfa0
Damien Erambert <damien@erambert.me>
2023-01-16 21:05:47 -0800

    :shirt:

--- src/helpers/mastodonContext.tsx
+++ src/helpers/mastodonContext.tsx
@@ -1,4 +1,3 @@
-import type { mastodon } from "masto";
 import { login } from "masto";
 import type { PropsWithChildren } from "react";
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
