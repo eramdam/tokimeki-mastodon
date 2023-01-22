d49a13f3e8dc80f2b2f6039304dd1656acf885db
Damien Erambert <damien@erambert.me>
2023-01-22 01:27:48 -0800

    Update _app.tsx
    https://github.com/adobe/react-spectrum/issues/3490

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,15 @@ import "../styles/globals.css";
 
 import { type AppType } from "next/dist/shared/lib/utils";
 import Head from "next/head";
-import { SSRProvider } from "react-aria";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <SSRProvider>
+    <>
       <Head>
         <title>Tokimeki Mastodon</title>
       </Head>
       <Component {...pageProps} />
-    </SSRProvider>
+    </>
   );
 };
 
