446d92737d577fecc1014c39e383f4b1c024115d
Damien Erambert <damien@erambert.me>
2024-02-24 10:18:53 -0800

    umami

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 
 import { type AppType } from "next/dist/shared/lib/utils";
 import Head from "next/head";
+import Script from "next/script";
 import { SSRProvider } from "react-aria";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
@@ -9,12 +10,12 @@ const MyApp: AppType = ({ Component, pageProps }) => {
     <SSRProvider>
       <Head>
         <title>Tokimeki Mastodon</title>
-        <script
-          async
-          src="https://umami-ochre-two.vercel.app/script.js"
-          data-website-id="51e9d456-88f4-4589-99b4-26eed5a94055"
-        ></script>
       </Head>
+      <Script
+        async
+        src="https://umami-ochre-two.vercel.app/script.js"
+        data-website-id="51e9d456-88f4-4589-99b4-26eed5a94055"
+      ></Script>
       <Component {...pageProps} />
     </SSRProvider>
   );
