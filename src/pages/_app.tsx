d760b799a7528f4e53a5a5db5b87d0dd2c011011
Damien Erambert <damien@erambert.me>
2024-03-08 23:39:58 -0800

    umami

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
       </Head>
       <Script
         async
-        src="https://umami-ochre-two.vercel.app/script.js"
+        src="https://umami.erambert.me/script.js"
         data-website-id="51e9d456-88f4-4589-99b4-26eed5a94055"
       ></Script>
       <Component {...pageProps} />
