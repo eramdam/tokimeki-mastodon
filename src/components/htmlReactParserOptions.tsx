34dcdef8d5242177456c828b7a3f2b94d454fef9
Damien Erambert <damien@erambert.me>
2024-08-18 15:52:37 -0700

    html-react-parser

--- src/components/htmlReactParserOptions.tsx
+++ src/components/htmlReactParserOptions.tsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import type { Text } from "domhandler";
-import type { HTMLReactParserOptions } from "html-react-parser";
+import type { DOMNode, HTMLReactParserOptions } from "html-react-parser";
 import { attributesToProps, domToReact, Element } from "html-react-parser";
 import type { mastodon } from "masto";
 import { mergeProps } from "react-aria";
@@ -33,13 +33,15 @@ export function getParserOptions(options: GetParserOptionsProps) {
             rel: "noreferrer noopener",
           });
           return (
-            <a {...anchorProps}>{domToReact(domNode.children, parseOptions)}</a>
+            <a {...anchorProps}>
+              {domToReact(domNode.children as DOMNode[], parseOptions)}
+            </a>
           );
         }
         if (domNode.tagName === "p") {
           return (
             <p className={clsx("leading-normal last:mb-0", classNames?.p)}>
-              {domToReact(domNode.children, parseOptions)}
+              {domToReact(domNode.children as DOMNode[], parseOptions)}
             </p>
           );
         }
