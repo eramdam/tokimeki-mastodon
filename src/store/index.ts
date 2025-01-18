a7e36b058109cf016db5cb3cbd56266198aa9166
Damien Erambert <damien@erambert.me>
2025-01-18 10:59:38 -0800

    use indexedDB for storage instead of localStorage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,9 @@
 import { omit, pick } from "lodash-es";
 import type { mastodon } from "masto";
 import { createWithEqualityFn } from "zustand/traditional";
-import { devtools, persist } from "zustand/middleware";
+import type { StateStorage } from "zustand/middleware";
+import { createJSONStorage, devtools, persist } from "zustand/middleware";
+import { get, set, del } from "idb-keyval";
 
 export enum SortOrders {
   OLDEST = "oldest",
@@ -69,6 +71,22 @@ export interface TokimekiState {
   lists: mastodon.v1.List[];
 }
 
+// Custom storage object
+const storage: StateStorage = {
+  getItem: async (name: string): Promise<string | null> => {
+    console.log(name, "has been retrieved");
+    return (await get(name)) || null;
+  },
+  setItem: async (name: string, value: string): Promise<void> => {
+    console.log(name, "with value", value, "has been saved");
+    await set(name, value);
+  },
+  removeItem: async (name: string): Promise<void> => {
+    console.log(name, "has been deleted");
+    await del(name);
+  },
+};
+
 export const initialPersistedState: TokimekiState = {
   settings: {
     sortOrder: SortOrders.OLDEST,
@@ -94,6 +112,7 @@ export const usePersistedStore = createWithEqualityFn<TokimekiState>()(
         return omit(state, ["actions", "nextAccount", "nextRelationship"]);
       },
       version: 3,
+      storage: createJSONStorage(() => storage),
     }),
     { name: "main-store" },
   ),
